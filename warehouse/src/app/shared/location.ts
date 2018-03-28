53ee9570976d2ed106f45723c8c6fa4303f5c24a
Jonathan Hawbecker <jhawbecker@pa.gov>
2018-03-27 20:50:57 -0400

    Class Lesson/Lab 4

---
+++ warehouse/src/app/shared/location.ts
@@ -0,0 +1,8 @@
+import { Product } from './product';
+export class Location {
+    locationID: string;
+    description: string;
+    productID: number;
+    quantity: number;
+    product: Product;
+}
---
+++ warehouse/src/app/shared/order.ts
@@ -0,0 +1,19 @@
+import { OrderLine } from './orderLine';
+import { Customer } from './customer';
+export class Order {
+    orderID: number;
+    customerID: number;
+    status: number;
+    orderDate: Date;
+    shipVia: number;
+    shipping: number;
+    tax: number;
+    shipName: string;
+    shipAddress: string;
+    shipCity: string;
+    shipRegion: string;
+    shipPostalCode: string;
+    shipCountry: string;
+    lines: Array<OrderLine>;
+    customer: Customer;
+}
